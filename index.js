6e5000c68d126ee27e4961ef01021b6ca0456925
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-12-13 09:05:32 +0800

    feat: first attempt

--- index.js
+++ index.js
@@ -1,24 +1,63 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dns = require("node:dns");
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+// TODO: use db
+const urls = [];
+
+app.post("/api/shorturl", (req, res) => {
+  const originalUrl = req.body.url;
+  const urlObject = new URL(originalUrl);
+  const hostname = urlObject.hostname;
+  dns.lookup(hostname, (err) => {
+    if (err) {
+      res.json({ error: "invalid url" });
+      return;
+    }
+    let index = urls.findIndex((url) => url === urlObject.href);
+    if (index === -1) {
+      index = urls.length;
+      urls.push(urlObject.href);
+    }
+    res.json({
+      original_url: urlObject.href,
+      short_url: index,
+    });
+  });
+});
+
+app.get("/api/shorturl/:id", (req, res) => {
+  const shortUrl = Number.parseInt(req.params.id);
+  if (
+    shortUrl === null ||
+    shortUrl === undefined ||
+    shortUrl > urls.length - 1
+  ) {
+    res.sendStatus(404);
+    return;
+  }
+  res.redirect(urls[shortUrl]);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
